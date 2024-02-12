722e4c4003e34fef1a90108cb9a09ff577ef3f1e
Criistiian95 <nehuen_cf_95@hotmail.com>
2024-02-12 13:02:38 -0300

    cambios en app

--- backend/database/config/config.js
+++ backend/database/config/config.js
@@ -2,11 +2,11 @@ require('dotenv').config();
 
 module.exports = {
   "development": {
-    "username": process.env.DB_USER || "root",
-    "password": process.env.DB_PASSWORD || null,
-    "database": process.env.DB_NAME || "sismed",
-    "host": process.env.DB_HOST || "localhost",
-    "port": process.env.DB_PORT || 3306,
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
+    "port": process.env.DB_PORT,
     "dialect": "mysql"
   },
   "test": {
