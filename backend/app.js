672bd4a7048bc5b06e8ef7a37bc6793c9290334c
Criistiian95 <nehuen_cf_95@hotmail.com>
2024-02-12 11:06:21 -0300

    cambios en app

--- backend/app.js
+++ backend/app.js
@@ -15,16 +15,26 @@ const ShiftRoutes = require("../backend/src/routes/shift")
 const cors= require("cors")
 const app = express();
 
+var corsOptions = {
+  origin: "http://localhost:3000",
+  credentials: true, 
+};
 
 
 
 
 
+let allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', "http://localhost:3001","http://localhost:3003","https://sistema-de-turnos-production-e4d9.up.railway.app/");
+    res.header("Access-Control-Allow-Methods", "OPTIONS, POST, GET, PUT, DELETE");
+    res.header('Access-Control-Allow-Headers', "*");
+    res.header('Access-Control-Allow-Credentials', true); // Permite cookies
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With")
+    next();
+  }
 
-  app.use(cors({
-    origin: "http://localhost:3001",
-    credentials: true // Habilitar el intercambio de cookies entre el cliente y el servidor
-  }));
+  app.use(cors(corsOptions));
 
 
 
@@ -42,7 +52,7 @@ app.use(expressSession({
   }
 }));
 app.use(cookieParser());
-
+app.use(allowCrossDomain);
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use(methodOverride('_method'));
@@ -53,6 +63,11 @@ app.use((req, res, next) => {
   app.locals.user = req.session.user; // Asigna los datos del usuario a app.locals.user
   next();
 });
+
+
+
+
+
 
 
 
@@ -74,6 +89,10 @@ app.use((req, res, next) => {
   res.send("Hola mundo");
   next()
 });
+
+
+
+
 
 
 
